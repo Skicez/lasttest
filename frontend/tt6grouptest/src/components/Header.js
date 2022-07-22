1be88dcf0f1f9ec836b44041d1d19dc979503f78
dtan089 <darrintanyz@gmail.com>
2022-07-22 11:15:01 +0800

    upload

--- frontend/tt6grouptest/src/components/Header.js
+++ frontend/tt6grouptest/src/components/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
           </li>
 
           <li>
-            <Link to="/registration">Registration</Link>
+            <Link to="/registration">Login</Link>
           </li>
 
           <li>
